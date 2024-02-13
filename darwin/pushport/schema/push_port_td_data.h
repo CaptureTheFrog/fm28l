a02fde011e2e48fe209fb241d9b8ef4390570f32
CaptureTheFrog <capturethefrog@proton.me>
2024-02-13 21:24:37 +0000

    Add TrackingID

--- darwin/pushport/schema/push_port_td_data.h
+++ darwin/pushport/schema/push_port_td_data.h
@@ -16,4 +16,14 @@ typedef struct {
     TDBerthIDType berth;
 } FullTDBerthID;
 
+/**
+ * \struct TrackingID
+ * \brief Indicate a corrected Tracking ID (headcode) for a service in a TD berth.
+ */
+typedef struct {
+    FullTDBerthID berth; ///< The TD berth where the incorrectly reported train has been identified to be. Note that this berth is that which was reported to Darwin and there is no guarantee that the train is still in this berth at any subsequent point in time.
+    TrainIdType incorrectTrainID; ///< The incorrect Train ID (headcode) that is being reported by TD.NET.
+    TrainIdType correctTrainID; ///< The correct Train ID (headcode) that should be reported by TD.NET.
+} TrackingID;
+
 #endif //FM28L_PUSH_PORT_TD_DATA_H
