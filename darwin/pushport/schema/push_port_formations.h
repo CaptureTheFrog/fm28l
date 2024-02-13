3fc044cbe49977b563ddd4d7ec5353e448f393b1
CaptureTheFrog <capturethefrog@proton.me>
2024-02-13 12:40:49 +0000

    Add CoachLoadingData and Loading types

--- darwin/pushport/schema/push_port_formations.h
+++ darwin/pushport/schema/push_port_formations.h
@@ -48,4 +48,29 @@ typedef struct{
     uint32_t formationCount; ///< Number of formations. Theoretically infinite but limited to 32-bit unsigned int in this implementation
 } ScheduleFormations;
 
+/**
+ * \struct CoachLoadingData
+ * \brief Type describing all of the Train Formations set for a Schedule.
+ */
+typedef struct{
+    LoadingValue loadingValue;
+    CoachNumberType coachNumber; ///< The number/identifier for this coach, e.g. "A".
+    char** srcString; ///< Pointer to string for the source of the loading data. Must be freed after use.
+    uint32_t srcLen; ///< Length of string for the source of the loading data including null terminator. Theoretically infinite but limited to 32-bit unsigned int in this implementation
+    SourceTypeInst srcInst; ///< The RTTI instance ID of the src (if any).
+} CoachLoadingData;
+
+/**
+ * \struct Loading
+ * \brief Loading data for an individual location in a schedule linked to a formation.
+ */
+typedef struct{
+    FormationIDType fid; ///< The unique identifier of the formation data.
+    RIDType rid; ///< RTTI unique Train ID
+    TIPLOCType tpl; ///< TIPLOC where the loading data applies.
+    CircularTimes circularTimes;
+    CoachLoadingData* loadings; ///< Array of loading data for an individual coach in the formation. If no loading data is provided for a coach in the formation then it should be assumed to have been cleared. Must be freed after use.
+    uint32_t loadingCount; ///< Number of loading data items. Theoretically infinite but limited to 32-bit unsigned int in this implementation
+} Loading;
+
 #endif //FM28L_PUSH_PORT_FORMATIONS_H
