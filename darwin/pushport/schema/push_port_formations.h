e63c22df675e00839e58fcbc2e4e476261034d36
CaptureTheFrog <capturethefrog@proton.me>
2024-02-13 11:40:31 +0000

    Add Formation

--- darwin/pushport/schema/push_port_formations.h
+++ darwin/pushport/schema/push_port_formations.h
@@ -26,4 +26,16 @@ typedef struct{
     uint32_t coachCount; ///< Number of coaches. Theoretically infinite but limited to 32-bit unsigned int in this implementation
 } CoachList;
 
+/**
+ * \struct Formation
+ * \brief Type describing a Train Formation for a Schedule.
+ */
+typedef struct{
+    CoachList coaches; ///< A list of coaches in this formation.
+    FormationIDType fid; ///< The unique identifier of this formation data.
+    char** srcString; ///< Pointer to string for the source of the formation data. Must be freed after use.
+    uint32_t srcLen; ///< Length of string for the source of the formation data including null terminator. Theoretically infinite but limited to 32-bit unsigned int in this implementation
+    SourceTypeInst srcInst; ///< The RTTI instance ID of the src (if any).
+} Formation;
+
 #endif //FM28L_PUSH_PORT_FORMATIONS_H
