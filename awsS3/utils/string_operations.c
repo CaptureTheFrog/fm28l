b184c7b5f2844d2402d82b0b212b0eef011511fa
CaptureTheFrog <capturethefrog@proton.me>
2024-02-14 23:29:21 +0000

    Implement awsS3_utils_trim

--- awsS3/utils/string_operations.c
+++ awsS3/utils/string_operations.c
@@ -5,9 +5,11 @@
 #include <malloc.h>
 #include <openssl/sha.h>
 #include <openssl/hmac.h>
+#include <ctype.h>
+#include <string.h>
 
 void awsS3_utils_lowercase(char* string, size_t n){
-    for(size_t i = 0; i < n - 1; i++)
+    for(size_t i = 0; i < n; i++)
         if(string[i] >= 'A' && string[i] <= 'Z')
             string[i] += ('a' - 'A');
 }
@@ -33,3 +35,38 @@ char* awsS3_utils_hmac_sha256(const unsigned char* input, size_t input_count, co
     HMAC(EVP_sha256(), key, key_count, input, input_count, hmac, NULL);
     return awsS3_utils_hex(hmac, SHA256_DIGEST_LENGTH);
 };
+
+int awsS3_utils_trim(char* string, size_t n, size_t* new_n){
+    char* first_non_space;
+    for(first_non_space = string; first_non_space < string + n; first_non_space++){
+        if(isspace(*first_non_space) == 0)
+            break;
+    }
+
+    char* last_non_space;
+    for(last_non_space = string + n - 1; last_non_space >= string; last_non_space--){
+        if(isspace(*last_non_space) == 0)
+            break;
+    }
+
+    if(last_non_space < first_non_space){
+        memset(string, 0, n);
+        return 0;
+    }
+
+    char* temp = malloc(last_non_space - first_non_space + 2);
+    if(temp == NULL)
+        return 1;
+
+    strncpy(temp, first_non_space,last_non_space - first_non_space + 1);
+    temp[last_non_space - first_non_space + 1] = '\0';
+
+    memset(string, 0, n);
+    strncpy(string, temp, last_non_space - first_non_space + 1);
+    free(temp);
+
+    if(new_n != NULL)
+        *new_n = last_non_space - first_non_space + 1;
+
+    return 0;
+}
--- awsS3/utils/string_operations.h
+++ awsS3/utils/string_operations.h
@@ -11,5 +11,6 @@ void awsS3_utils_lowercase(char* string, size_t n);
 char* awsS3_utils_hex(const unsigned char* input, size_t count);
 char* awsS3_utils_sha256hash(const unsigned char* input, size_t count);
 char* awsS3_utils_hmac_sha256(const unsigned char* input, size_t input_count, const unsigned char* key, size_t key_count);
+int awsS3_utils_trim(char* string, size_t n, size_t* new_n);
 
 #endif //FM28L_STRING_OPERATIONS_H
